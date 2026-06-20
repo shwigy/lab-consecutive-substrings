2c790cde03d4e2316998ba04274ffb9483139c05
shwigy <floressj04@icloud.com>
2026-06-20 03:02:15 -0400

    Finalized 'consecutive_substrings.js' with code that fulfills assignment requirements. Additionally added a few log testing near the bottom of the file's code

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,18 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  const result = [];
+ 
+  // outer loop, picks the STARTING character of each substring
+  for (let i = 0; i < string.length; i++) {
+ 
+    for (let j = i; j < string.length; j++) {
+ 
+      // slice(i, j + 1) grabs characters from index i up to AND including j
+      result.push(string.slice(i, j + 1));
+    }
+  }
+ 
+  return result;
 }
 
 if (require.main === module) {
@@ -11,6 +24,16 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log("");
+ 
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(''));
+ 
+  console.log("");
+ 
+  console.log("Expecting: ['a', 'ab', 'b']");
+  console.log("=>", consecutiveSubstrings('ab'));
 }
 
 module.exports = consecutiveSubstrings;
